7aef2443c7f8bfad07870f6aac259082a733e698
Nuraiym Obozkanova <nuraika.obozkanova@gmail.com>
2023-06-22 17:26:43 +0600

    fixed genre test

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -4,50 +4,65 @@ import pytest
 from typing import Coroutine, Dict, Union
 
 from ..settings import test_settings as sett
-from ..testdata.genre import ES_GENRE_GEN_DATA
-from ..testdata.film import UUIDS_FILMS, ES_FILM_SEARCH_GEN_DATA
+from ..testdata.genre import (
+    UUIDS_GENRES,
+    ES_GENRE_GEN_DATA,
+    ES_GENRES_PARAMETRIZE_POSITIVE_DATA,
+    ES_GENRES_PARAMETRIZE_NEGATIVE_DATA,
+    ES_GENRE_BY_ID_PARAMETRIZE_POSITIVE_DATA,
+    ES_GENRE_BY_ID_PARAMETRIZE_NEGATIVE_DATA
+)
 
+@pytest.mark.parametrize(
+    'query_data, expected_answer',
+    ES_GENRES_PARAMETRIZE_POSITIVE_DATA + ES_GENRES_PARAMETRIZE_NEGATIVE_DATA
+)
 
 @pytest.mark.anyio
 async def test_get_all_genres(
 	make_get_request: Coroutine,
 	es_write_data: Coroutine,
+	query_data: Dict[str, Union[str, int, float, None]],
+	expected_answer: Dict[str, Union[str, int, float, None]],
 ):
-	# Load data into ES
-	await es_write_data(data=ES_GENRE_GEN_DATA, index='genres')
 
-	# Make a request
+
 	url = f'{sett.app_api_host}genres'
-	response = await make_get_request(url, params={})
+	response = await make_get_request(url, params=query_data)
 	data_response = await response.json()
 
-	# Check tests
-	assert response.status == 200
-	assert len(data_response) == len(ES_GENRE_GEN_DATA)
-	for i, genre in enumerate(data_response):
-		assert genre['id'].lower() == str(ES_GENRE_GEN_DATA[i]['id']).lower()
-		assert genre['name'] == ES_GENRE_GEN_DATA[i]['name']
+	assert response.status == expected_answer.get('status')
+	assert len(data_response) == expected_answer.get('length')
+	if response.status == 422:
+		assert data_response.get('detail')[0].get('msg') == expected_answer.get('msg')
+
+	if 'full_return' in expected_answer.keys():
+		assert data_response[0].get('name') == expected_answer.get('full_return').get('name')
+		assert data_response[0].get('description') == expected_answer.get('full_return').get('description')
+
 
 
 @pytest.mark.parametrize(
-	'genre_id, expected_status',
-	[
-		(str(genre['id']), 200) for genre in ES_GENRE_GEN_DATA
-	]
+    ('genre_id', 'expected_status'),
+    ES_GENRE_BY_ID_PARAMETRIZE_POSITIVE_DATA + ES_GENRE_BY_ID_PARAMETRIZE_NEGATIVE_DATA
 )
 @pytest.mark.anyio
 async def test_get_genre_by_id(
-	make_get_request: Coroutine,
-	es_write_data: Coroutine,
-	genre_id: str,
-	expected_status: int,
+    make_get_request: Coroutine,
+    es_write_data: Coroutine,
+    genre_id: Dict[str, Union[str, int, float, None]],
+    expected_status: Dict[str, Union[str, int, float, None]]
 ):
-	# Load data into ES
-	await es_write_data(data=ES_GENRE_GEN_DATA, index='genres')
 
-	# Make a request
-	url = f'{sett.app_api_host}genres/{genre_id}'
-	response = await make_get_request(url, params={})
+    url = f'{sett.app_api_host}genres/{genre_id.get("genre_id")}'
+    response = await make_get_request(url, '')
+    data_response = await response.json()
 
-	# Check tests
-	assert response.status == expected_status
+    assert response.status == expected_status.get('status')
+    assert len(data_response) == expected_status.get('length')
+    if response.status == 404:
+        assert data_response.get('detail')[0].get('msg') == expected_status.get('msg')
+
+    if 'full_return' in expected_status.keys():
+        assert data_response.get('name') == expected_status.get('full_return').get('name')
+        assert data_response.get('description') == expected_status.get('full_return').get('description')
--- tests/functional/testdata/genre.py
+++ tests/functional/testdata/genre.py
@@ -1,353 +1,86 @@
 import uuid
 
 
+UUIDS_GENRES = [
+    uuid.UUID("0ce50db6-6c9f-4ffb-99d7-089497bc0a45"),
+    uuid.UUID("689b4bc0-e9c6-44aa-b14a-0eb7f91346ba"),
+    uuid.UUID("0ef7ba62-a99b-4c19-be18-3c09fd757362"),
+    uuid.UUID("a373648f-c455-4386-a63b-dd93afea14d5"),
+    uuid.UUID("b600c425-71e6-419e-8a45-e027cc65181a")
+
+]
+
 ES_GENRE_GEN_DATA = [
     {
         "id" : uuid.UUID('0fbcd2b3-2792-4468-8885-06d653f368c8'),
-        "name" : "Action",
+        "name" : "yeah",
         "description" : "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
     },
     {
         "id" : uuid.UUID('b29306f4-e843-4f6f-96c8-0e815a504575'),
-        "name" : "Sci-Fi",
+        "name" : "them",
         "description" : "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
     },
-    {
-      "id": uuid.UUID("1f99f93f-d422-4bdc-a1b1-844b4f9d4d3d"),
-      "name": "magazine",
-      "description": "Account report player ready. View head keep turn very to power. Together run against beyond.\nTonight and may exactly short. Skill force serve."
-    },
-    {
-      "id": uuid.UUID("689b4bc0-e9c6-44aa-b14a-0eb7f91346ba"),
-      "name": "yes",
-      "description": "Thank policy technology during respond pattern. Final notice key check.\nGuy staff develop. Be kid pattern skin behavior study keep win."
-    },
-    {
-      "id": uuid.UUID("0ef7ba62-a99b-4c19-be18-3c09fd757362"),
-      "name": "myself",
-      "description": "Scene turn call bank. In order professor clear. Way push soldier special. No power other plant guy role day."
-    },
-    {
-      "id": uuid.UUID("65bc1f4a-e2eb-4077-927e-c8a67148da62"),
-      "name": "tonight",
-      "description": "Case mission reality mother despite hit Mr. Sing crime relate view fine under positive. Herself office meeting top cold make."
-    },
-    {
-      "id": uuid.UUID("533672f2-5dc0-443a-a21a-00967d47ba5a"),
-      "name": "guess",
-      "description": "Yes author position spend assume create record.\nDescribe than strong. Difficult price still nearly. Case send then hit image decade foot."
-    },
-    {
-      "id": uuid.UUID("045eff47-0cf3-4095-9dc4-711e5e7f9796"),
-      "name": "each",
-      "description": "Use surface hour city dinner seat network guy. Professor cause perform current education edge prevent.\nTalk wonder history card positive sea break."
-    },
-    {
-      "id": uuid.UUID("3692dfce-6dc9-4cf7-9e1f-67ec174eac7b"),
-      "name": "poor",
-      "description": "Rest especially like message institution detail involve. Feel doctor indeed market compare expert structure. Ok natural report worry enough college."
-    },
-    {
-      "id": uuid.UUID("ac109604-37cf-4a1c-8f58-cb6ee9ffe338"),
-      "name": "sing",
-      "description": "Wonder almost nor study town. Suggest both hand learn thing group.\nBillion six throughout measure treat. Oil miss experience daughter heart Mr similar."
-    },
-    {
-      "id": uuid.UUID("e822c3f0-de50-41a1-a096-0eaacb45e500"),
-      "name": "certainly",
-      "description": "Degree finish mouth choice letter three performance from. Reveal film could own floor challenge card."
-    },
-    {
-      "id": uuid.UUID("8c2df934-a207-4ac4-9be3-ed553df9a205"),
-      "name": "social",
-      "description": "Building road these prove.\nCulture wear white goal hope store region. Firm college law card plan many."
-    },
-    {
-      "id": uuid.UUID("3f360503-9d3b-4801-9f37-e518350e5432"),
-      "name": "others",
-      "description": "Who bank might home western lose newspaper. Realize pattern task south population."
-    },
-    {
-      "id": uuid.UUID("e3399242-38bc-40f9-b183-c4a590919c4c"),
-      "name": "vote",
-      "description": "Field make many. What interview put themselves natural information animal relationship. Present throughout health interview cup."
-    },
-    {
-      "id": uuid.UUID("94b1a87a-9d1d-4a14-9549-fe2712b4d38a"),
-      "name": "trade",
-      "description": "Blood majority scientist test. Fire traditional among many table drop ground of."
-    },
-    {
-      "id": uuid.UUID("db6d36c3-a22d-4019-a6d0-d0a93438ab51"),
-      "name": "require",
-      "description": "Make probably drug view forward book foot. Well successful our seat.\nCarry rule very between. Western day play agency. At indicate only early."
-    },
-    {
-      "id": uuid.UUID("7b244858-600b-421a-8540-45094cdb4f70"),
-      "name": "camera",
-      "description": "Authority ask training after lot man. Task movie measure whether pass left."
-    },
-    {
-      "id": uuid.UUID("f50b130c-b063-48aa-a700-a7cb7125aad1"),
-      "name": "old",
-      "description": "Near finish edge and action truth. While hour south magazine clearly section stock kid. Attack short while management."
-    },
-    {
-      "id": uuid.UUID("57fe7013-cb53-407a-95af-9e4da91b3678"),
-      "name": "customer",
-      "description": "Spend enter raise. Sea phone everybody whether happen.\nHim truth provide account brother woman memory. Religious most administration poor."
-    },
-    {
-      "id": uuid.UUID("6c4ae096-3f0e-4fc6-a435-d4463291f216"),
-      "name": "agree",
-      "description": "Heavy together concern. Many sing others upon.\nElse than forward television reflect general dark."
-    },
-    {
-      "id": uuid.UUID("e3f29655-f767-4409-9162-50a9c65243ec"),
-      "name": "ability",
-      "description": "Themselves management PM class. However camera kid night. Specific relationship life arm."
-    },
-    {
-      "id": uuid.UUID("9204c398-b85f-42c8-87ff-477ff36b6212"),
-      "name": "onto",
-      "description": "Consider others rate hold. Coach impact southern various political training history. Management deal contain soldier."
-    },
-    {
-      "id": uuid.UUID("8e3aa38a-5314-4c20-bf5e-61a0844f9365"),
-      "name": "last",
-      "description": "Development knowledge computer mother respond summer. No life strong power follow system so."
-    },
-    {
-      "id": uuid.UUID("bc8736b1-43b9-4848-8e16-f2b8a78c9e1c"),
-      "name": "happy",
-      "description": "Third agree clear wish agent leader player. Public carry certainly sense. Sell stage ability according shake power else.\nHope the dinner detail."
-    },
-    {
-      "id": uuid.UUID("63123aba-a2d9-496a-ae6a-4082bb8c9d21"),
-      "name": "try",
-      "description": "Fish push more trial fast according since. Race individual interest result.\nNumber study time stuff power."
-    },
-    {
-      "id": uuid.UUID("49c2f24d-5f87-4a4e-ac8e-37d3fa091a13"),
-      "name": "subject",
-      "description": "Commercial enough everyone owner player face. Race catch about meeting present new. Pressure necessary suffer forward likely."
-    },
-    {
-      "id": uuid.UUID("3b42c55e-fbb9-4d38-a9eb-4ff1555d12c8"),
-      "name": "structure",
-      "description": "Ball young be hit. Audience many it act impact."
-    },
-    {
-      "id": uuid.UUID("f89dd7bb-7cb5-45e5-baba-d518d27e1535"),
-      "name": "cost",
-      "description": "Must paper performance lose. Note one wall truth official. Difference throughout full."
-    },
-    {
-      "id": uuid.UUID("c8277309-0db7-4152-891a-27c958d9cde9"),
-      "name": "step",
-      "description": "Magazine effect ok blood game political. Summer style return final. Point type wide anyone instead government.\nReduce challenge student. Indicate stay whom."
-    },
-    {
-      "id": uuid.UUID("8f32ba28-ac81-4afb-8666-2b674f5adabd"),
-      "name": "open",
-      "description": "Organization news side language sport hand lot. Story decision can west. Theory machine character where ball."
-    },
-    {
-      "id": uuid.UUID("94b6ce05-df99-4131-8b00-56772521d910"),
-      "name": "magazine",
-      "description": "Week authority sea father first even politics laugh. Might back already. Heart western data near."
-    },
-    {
-      "id": uuid.UUID("e86209fa-850e-4845-88a8-8ce4b0a2c65c"),
-      "name": "entire",
-      "description": "Thus run garden like Congress then girl. Radio fly thus whom unit.\nInvolve few sit challenge his. Truth person model team task."
-    },
-    {
-      "id": uuid.UUID("804238a9-c2c8-46a6-8a9b-b54b1f6c7377"),
-      "name": "carry",
-      "description": "Rate rule tax test indicate. Could hear front meeting.\nPhysical theory woman west coach onto identify. From bill home character charge off."
-    },
-    {
-      "id": uuid.UUID("f6bd9c29-ba54-41f6-8e2f-04d1e5de18da"),
-      "name": "return",
-      "description": "Early share father including education follow. Information record ground major month nation collection.\nReport its visit woman difference role."
-    },
-    {
-      "id": uuid.UUID("ef2d4c5f-2124-41e9-b84e-1bcb4273b149"),
-      "name": "believe",
-      "description": "Partner star represent compare girl security focus. Test participant hold person technology necessary create."
-    },
-    {
-      "id": uuid.UUID("d7b64f5f-4e2f-4bb9-8cd6-9308f7c0e0da"),
-      "name": "stage",
-      "description": "Cost onto none break fly force pick how."
-    },
-    {
-      "id": uuid.UUID("3974a20f-2d19-4af4-aa98-41eb39d7852a"),
-      "name": "large",
-      "description": "Republican if walk cell whether attack. Chair science word away. Daughter miss leave charge.\nSeat son in eight. Them born foot save focus."
-    },
-    {
-      "id": uuid.UUID("cb1ccc03-ef49-47d8-b95e-ea974e45cde4"),
-      "name": "list",
-      "description": "Loss news produce per. Argue speech watch occur ability. Word subject positive coach maintain."
-    },
-    {
-      "id": uuid.UUID("f7041196-8d31-4d6b-bfe7-1738814a554e"),
-      "name": "Mr",
-      "description": "Several part deal chair. House charge avoid medical them."
-    },
-    {
-      "id": uuid.UUID("3555b78d-54b3-40fe-86fc-00edc88a18ee"),
-      "name": "fill",
-      "description": "He fact argue discuss reduce. Simple ready development pressure. Power kind lay art cold process town hotel."
-    },
-    {
-      "id": uuid.UUID("faf1a051-3c17-4f04-9810-beaa9902768d"),
-      "name": "customer",
-      "description": "Lot sound have laugh government season turn. Buy suggest claim wonder often citizen. Career skill production final. Some hand among heart change."
-    },
-    {
-      "id": uuid.UUID("52116f66-f81a-4997-80ca-c1be268ffe21"),
-      "name": "rise",
-      "description": "Agreement air positive get action rich. Heavy of field. Type hope everything."
-    },
-    {
-      "id": uuid.UUID("974d5e97-00a5-4059-93c8-0ce2d23b3d12"),
-      "name": "feeling",
-      "description": "Draw lot pressure. Game argue statement movie nice woman two."
-    },
-    {
-      "id": uuid.UUID("3d4abc15-3819-47d7-9d5a-039ac83d582f"),
-      "name": "organization",
-      "description": "Week hold thank cut movie. Fight action none. Show head as over three.\nCase history significant nice man them."
-    },
-    {
-      "id": uuid.UUID("b4355f49-a90d-4bcd-865e-c370958e7464"),
-      "name": "girl",
-      "description": "Who conference long international approach voice learn stock. Certain player specific already."
-    },
-    {
-      "id": uuid.UUID("45988fbb-81c9-4ed6-bfe1-cd62acdcc241"),
-      "name": "box",
-      "description": "Produce significant remain. Citizen involve follow fish low."
-    },
-    {
-      "id": uuid.UUID("42a6025b-3d1f-48cf-a4ee-9ce32731dc81"),
-      "name": "make",
-      "description": "Rise relationship moment page ago protect. Require trip often early continue. Easy collection high hard cup lay man. Family court gun reality community game."
-    },
-    {
-      "id": uuid.UUID("6fe4a650-26f3-46d6-a469-99907da2b03f"),
-      "name": "memory",
-      "description": "Marriage first coach modern tonight list. Institution range general. Kitchen commercial thank. Structure safe while and large."
-    },
-    {
-      "id": uuid.UUID("82cb1671-e490-4755-897c-1902941c2044"),
-      "name": "poor",
-      "description": "Truth friend suffer piece go he child. Newspaper father until pay already chair. Term about can similar sell seat well.\nDecide age far despite."
-    },
-    {
-      "id": uuid.UUID("883db78c-f163-48df-985e-ed2492edd3f6"),
-      "name": "trial",
-      "description": "Coach contain seat everybody miss surface. Daughter our various. Opportunity role town yet measure."
-    },
+
 ]
-#
-# import uuid
-#
-# ES_GENRE_SEARCH_PARAMETRIZE_POSITIVE_DATA = [
-#     (
-#         'genre',
-#         {'query': 'Action', 'page_number': 0, 'page_size': 50},
-#         {'status': 200, 'length': 50}
-#     ),
-#     (
-#         'genre',
-#         {'query': 'Sci-Fi', 'page_number': 1, 'page_size': 2},
-#         {'status': 200, 'length': 2}
-#     ),
-#     (
-#         'genre',
-#         {'query': 'Drama'},
-#         {'status': 200, 'length': 50}
-#     ),
-#     (
-#         'genre',
-#         {'query': 'Thriller', 'page_number': 0, 'page_size': 1},
-#         {'status': 200, 'length': 1, 'full_return': [
-#             {
-#                 "id": "1",
-#                 "name": "Thriller"
-#             }
-#         ]}
-#     )
-# ]
-#
-# ES_GENRE_SEARCH_PARAMETRIZE_NEGATIVE_DATA = [
-#     (
-#         'genre',
-#         {'query': 'Mystery', 'page_number': 0, 'page_size': 50},
-#         {'status': 404, 'length': 1}
-#     ),
-#     (
-#         'genre',
-#         {'query': 'Fantasy', 'page_number': -1, 'page_size': 50},
-#         {'status': 422, 'length': 1, 'msg': 'ensure this value is greater than or equal to 0'}
-#     ),
-#     (
-#         'genre',
-#         {'query': 'Horror', 'page_size': 1000000},
-#         {'status': 422, 'length': 1, 'msg': 'ensure this value is less than or equal to 500'}
-#     )
-# ]
-#
-# ES_GENRE_SEARCH_GEN_DATA = [
-#     {'id': uuid.UUID('0fbcd2b3-2792-4468-8885-06d653f368c8'), 'name': 'Action'},
-#     {'id': uuid.UUID('b29306f4-e843-4f6f-96c8-0e815a504575'), 'name': 'Sci-Fi'},
-#     {'id': uuid.UUID('3a2f4b0e-4561-4c12-9fd9-735a7394d59d'), 'name': 'Drama'},
-#     {'id': uuid.UUID('76b9085d-4373-46f6-9a5f-35fba7b2e37a'), 'name': 'Thriller'}
-# ]
-#
-# UUIDS_GENRES = [
-#     uuid.UUID("0fbcd2b3-2792-4468-8885-06d653f368c8"),
-#     uuid.UUID("b29306f4-e843-4f6f-96c8-0e815a504575"),
-#     uuid.UUID("3a2f4b0e-4561-4c12-9fd9-735a7394d59d"),
-#     uuid.UUID("76b9085d-4373-46f6-9a5f-35fba7b2e37a")
-# ]
-#
-# ES_GENRES_PARAMETRIZE_POSITIVE_DATA = [
-#     (
-#         {'sort_param': 'name', 'page_number': 0, 'page_size': 50},
-#         {'status': 200, 'length': 50}
-#     ),
-#     (
-#         {'page_number': 0, 'page_size': 50},
-#         {'status': 200, 'length': 50}
-#     ),
-#     (
-#         {'page_size': 50},
-#         {'status': 200, 'length': 50}
-#     ),
-#     (
-#         {'page_number': 0, 'page_size': 1},
-#         {'status': 200, 'length': 1, 'full_return': [
-#             {'id': str(genre_id), 'name': 'Genre 1'} for genre_id in UUIDS_GENRES
-#         ]}
-#     )
-# ]
-#
-# ES_GENRES_PARAMETRIZE_NEGATIVE_DATA = [
-#     (
-#         {'sort_param': 'unknown', 'page_number': 0, 'page_size': 50},
-#         {'status': 400, 'length': 1, 'msg': 'Invalid sort parameter'}
-#     ),
-#     (
-#         {'page_number': -1, 'page_size': 50},
-#         {'status': 422, 'length': 1, 'msg': 'ensure this value is greater than or equal to 0'}
-#     ),
-#     (
-#         {'page_size': 1000000},
-#         {'status': 422, 'length': 1, 'msg': 'ensure this value is less than or equal to 500'}
-#     )
-# ]
+
+
+ES_GENRES_PARAMETRIZE_POSITIVE_DATA = [
+    (
+        {'page_number': 0, 'page_size': 50},
+        {'status': 200, 'length': 50}
+    ),
+    (
+        {'page_number': 1, 'page_size': 50},
+        {'status': 200, 'length': 50}
+    ),
+]
+
+ES_GENRES_PARAMETRIZE_NEGATIVE_DATA = [
+    (
+        {'genre': '123', 'page_number': 0, 'page_size': 50},
+        {'status': 422, 'length': 1, 'msg': 'value is not a valid uuid'}
+    ),
+    (
+        {'genre': '0cc0ee04-e27c-4c5a-a157-a408e799b651', 'page_number': -1, 'page_size': 50},
+        {'status': 422, 'length': 1, 'msg': 'ensure this value is greater than or equal to 0'}
+    ),
+    (
+        {'genre': '0cc0ee04-e27c-4c5a-a157-a408e799b651', 'page_size': 1000000},
+        {'status': 422, 'length': 1, 'msg': 'ensure this value is less than or equal to 500'}
+    )
+]
+
+
+ES_GENRE_BY_ID_PARAMETRIZE_POSITIVE_DATA = [
+    (
+        {'genre_id': 'a373648f-c455-4386-a63b-dd93afea14d5'},
+        {'status': 200, 'length': 3, 'full_return' : {
+                "id": "1",
+                "name": "yeah",
+                "description": "Side ask need. Administration ok light agreement note end positive. Particular Congress letter fast she."
+            }
+        }
+    ),
+    (
+        {'genre_id': 'b600c425-71e6-419e-8a45-e027cc65181a'},
+        {'status': 200, 'length': 3, 'full_return' : {
+                "id": "1",
+                "name": "them",
+                "description": "Short success teacher sometimes provide. Born can impact shake later agree teach. Clear question city official attack. Attack prepare mind imagine a."
+            }
+        }
+    )
+]
+
+
+ES_GENRE_BY_ID_PARAMETRIZE_NEGATIVE_DATA = [
+    (
+        {'genre_id': '123'},
+        {'status': 404, 'length': 1, 'msg': 'value is not a valid uuid'}
+    ),
+    (
+        {'genre_id': 123},
+        {'status': 404, 'length': 1, 'msg': 'value is not a valid uuid'}
+    )
+]
