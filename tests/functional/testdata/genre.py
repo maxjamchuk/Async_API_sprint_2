466dee3c7abc2cbe76605743cb099cb76269d03f
Roman Romanov <roman.romanov@geniusto.com>
2023-06-23 00:24:03 +0400

    Fixed bugs version 2.0

--- tests/functional/testdata/genre.py
+++ tests/functional/testdata/genre.py
@@ -4,15 +4,14 @@ import uuid
 ES_GENRE_GEN_DATA = [
     {
         "id" : uuid.UUID('0fbcd2b3-2792-4468-8885-06d653f368c8'),
-        "name" : "yeah",
+        "name" : "Action",
         "description" : "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
     },
     {
         "id" : uuid.UUID('b29306f4-e843-4f6f-96c8-0e815a504575'),
-        "name" : "them",
+        "name" : "Sci-Fi",
         "description" : "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
     },
-
 ]
 
 
@@ -48,7 +47,7 @@ ES_GENRE_BY_ID_PARAMETRIZE_POSITIVE_DATA = [
         {'genre_id': '0fbcd2b3-2792-4468-8885-06d653f368c8'},
         {'status': 200, 'length': 3, 'full_return' : {
                 "id": "1",
-                "name": "yeah",
+                "name": "Action",
                 "description": "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
             }
         }
@@ -57,7 +56,7 @@ ES_GENRE_BY_ID_PARAMETRIZE_POSITIVE_DATA = [
         {'genre_id': 'b29306f4-e843-4f6f-96c8-0e815a504575'},
         {'status': 200, 'length': 3, 'full_return' : {
                 "id": "1",
-                "name": "them",
+                "name": "Sci-Fi",
                 "description": "Answer pick always. Organization nice force middle result well brother ask. Evening use agreement av... Behind blood by evidence learn parent accept."
             }
         }
